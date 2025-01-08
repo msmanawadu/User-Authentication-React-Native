5346fe02c0d2a5b30c46b98a08c310a23d021ac3
msmanawadu <msmanawadu@gmail.com>
2025-01-08 14:48:47 +0530

    Implementing Sign up and Sign in flows

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { Alert } from 'react-native';
 
 function SignupScreen() {
-  return <AuthContent />;
+	const [isAuthenticating, setIsAuthenticating] = useState(false); // To manage loading state
+
+	async function signupHandler({ email, password }) {
+		setIsAuthenticating(true);
+		try {
+			await createUser(email, password);
+		} catch (error) {
+			Alert.alert(
+				'Authentication Failed!',
+				'Could not create user. Please check your input and try again later!'
+			);
+		}
+		setIsAuthenticating(false);
+	}
+
+	if (isAuthenticating) {
+		return <LoadingOverlay message='Creating User...' />;
+	}
+
+	return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
---
+++ store/auth-context.js
@@ -0,0 +1,33 @@
+// Managing app-wide user authentication status
+
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext({
+	token: '',
+	isAuthenticated: false,
+	authenticate: (token) => {},
+	logout: () => {},
+});
+
+function AuthContextProvider({ children }) {
+	const [authToken, setAuthToken] = useState(); // To keep track of user's authentication status
+
+	function authenticate(token) {
+		setAuthToken(token);
+	}
+
+	function logout() {
+		setAuthToken(null);
+	}
+
+	const value = {
+		token: authToken,
+		isAuthenticated: !!authToken,
+		authenticate: authenticate,
+		logout: logout,
+	};
+
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContextProvider;
